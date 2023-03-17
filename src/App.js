e917c1f7fe295515ebd82e0366e77d9c41d2796e
sam <samscloud2@gmail.com>
2023-03-17 15:28:46 -0300

    play/pause bug fixed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 function App() {
-  /**
-   * this code has a bug
-   * the play/pause btn works properly 2 times, then stops working
-   * the bug will be fixed in the next commit. 
-   */
 
-  let audio1 = new Audio("https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3");
+  let audioRef = useRef(new Audio("https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3"));
 
   // create a state variable to keep track of whether the audio is playing or paused
   const [audioString, setIsPlaying] = useState('Play');
   // create an event handler to toggle the state of the isPlaying variable and play or pause the audio
   const handleAudioToggle = () => {
-    setIsPlaying(false)
+    let audio1 = audioRef.current; 
+
     if (audio1.paused) {
       audio1.play(); 
       setIsPlaying("Pause")
