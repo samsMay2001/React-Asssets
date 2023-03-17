e8d4f1c4a00badeea6e0267d6c434b13f5d52f26
sam <samscloud2@gmail.com>
2023-03-17 11:49:24 -0300

    added an image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import profilePic from './assets/images/profile pic.jpg'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Task: add three images with some styling 
+      </h1>
+      <img
+        height={200}
+        src = {profilePic}
+        alt = "An img of a Samuel"
+      />
     </div>
   );
 }
