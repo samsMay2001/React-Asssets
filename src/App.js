2a3a48dada93041bda71490b168624e6a81fd26e
sam <samscloud2@gmail.com>
2023-03-17 13:03:10 -0300

    added video 2 vids

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import profilePic from './assets/images/profile pic.jpg'
+import profileVid from "./assets/videos/Y2Mate.is - Joel's S14 Short  4K-fVr_yM5Q1vs-720p-1654298027861.mp4"
+import ReactPlayer from 'react-player';
 
 
 function App() {
+
+  let videoClassCss = {
+    display : 'flex', 
+    alignItems : 'center',
+    justifyContent : 'center' 
+  }
+  
   return (
     <div className="App">
       <h1>
-        Task: add three images with some styling 
+        Task: add 1 image and 2 videos
       </h1>
+      <h4>Fav Picture</h4>
       <img
         height={200}
         src = {profilePic}
         alt = "An img of a Samuel"
       />
+      <h4>Fav Video</h4>
+      <video src={profileVid} height = {200} controls/>
+      <h4>Fav YouTube Video</h4>
+      <div style={videoClassCss}>
+        <ReactPlayer height={200} width={400} url="https://www.youtube.com/watch?v=-621SEHkfIM&ab_channel=OVTL%CE%94W"/>
+      </div>
+      
     </div>
   );
 }
